1bff77f4f5349bbfd2f7aafe49d1cd21c3d7e8ac
Marymezh <maria.mezhova@yahoo.com>
2021-05-05 09:50:27 +0700

    ProfileView.swift file was created with outlets

---
+++ Netology_IB_Instruments/Netology_IB_Instruments/ProfileView.swift
@@ -0,0 +1,21 @@
+//
+//  ProfileView.swift
+//  Netology_IB_Instruments
+//
+//  Created by Maria Mezhova on 05.05.2021.
+//
+
+import UIKit
+
+class ProfileView: UIView {
+
+    @IBOutlet weak var userPicture: UIImageView!
+    
+    @IBOutlet weak var userName: UILabel!
+    
+    @IBOutlet weak var userBirthday: UILabel!
+    
+    @IBOutlet weak var userCity: UILabel!
+    
+    @IBOutlet weak var someText: UITextView!
+}
